065684179a2620cc0a6f178a2739e450a86789ab
qkrckstjq <cksk49@naver.com>
2025-03-10 17:38:56 +0900

    비교 연산자 코드 수정

---
+++ src/main/java/comparisonOperator/ComparisonOperatorMain.java
@@ -0,0 +1,29 @@
+package comparisonOperator;
+
+public class ComparisonOperatorMain {
+    public static void main(String[] args) {
+        String a = new String("test");
+        String b = new String("test");
+        Integer c = 1234;
+        Integer d = 1234;
+
+        twoComparison(a, b);
+        twoComparison(c, d);
+    }
+
+    protected static void twoComparison(Object a, Object b) {
+        System.out.println("== result");
+        if (a == b) {
+            System.out.println("same");
+        } else {
+            System.out.println("diff");
+        }
+
+        System.out.println("equals result");
+        if (a.equals(b)) {
+            System.out.println("same");
+        } else {
+            System.out.println("diff\n");
+        }
+    }
+}
