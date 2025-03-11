aae15737cf0b43fed80c0b7309ff167042fb9a87
qkrckstjq <cksk49@naver.com>
2025-03-11 18:59:37 +0900

    Polymorphism README.md 수정

--- src/main/java/JavaDefaultConcept/polymorphism/Animal.java
+++ src/main/java/JavaDefaultConcept/polymorphism/Animal.java
@@ -3,4 +3,8 @@ package JavaDefaultConcept.polymorphism;
 public interface Animal {
     static final boolean dieAble = true;
     void sound();
+
+    default void qwe() {
+        System.out.println("dlrp ehlspe");
+    }
 }
