6989863e6fc78dc32dda21bdaefeb30d1661f39b
qkrckstjq <cksk49@naver.com>
2025-03-25 20:48:27 +0900

    Reader & Writer 코드 추가

---
+++ src/main/java/javaDefaultConcept/readerWriter/ReaderMain.java
@@ -0,0 +1,43 @@
+package javaDefaultConcept.readerWriter;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class ReaderMain {
+    private static final String FILE = "./src/main/java/javaDefaultConcept/readerWriter/text.txt";
+
+    public static void main(String[] args) {
+        reading();
+
+        inputStreamReading();
+
+        bufferedReading();
+    }
+
+    public static void reading() {
+        try(FileReader reader = new FileReader(FILE)) {
+            int ch = reader.read();
+            System.out.println((char) ch);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void inputStreamReading() {
+        try(InputStreamReader reader = new InputStreamReader(new FileInputStream(FILE), StandardCharsets.UTF_8)) {
+            int ch = reader.read();
+            System.out.println((char) ch);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void bufferedReading() {
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(FILE), StandardCharsets.UTF_8))) {
+            String line = reader.readLine();
+            System.out.println(line);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/javaDefaultConcept/readerWriter/WriterMain.java
@@ -0,0 +1,44 @@
+package javaDefaultConcept.readerWriter;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class WriterMain {
+    private static final String FILE = "./src/main/java/javaDefaultConcept/readerWriter/text.txt";
+
+    public static void main(String[] args) {
+        writing();
+
+        inputStreamWriting();
+
+        bufferedWriting();
+    }
+
+    public static void writing() {
+        try(FileWriter writer = new FileWriter(FILE, true)) {
+            String txt = "\n이건 Writer로 입력하는 String입니다부디ㅏ문이";
+            writer.write(txt.toCharArray());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void inputStreamWriting() {
+        try(OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(FILE, true), StandardCharsets.UTF_8)) {
+            String txt = "\n이건 Writer로 입력하는 String입니다부디ㅏ문이";
+            writer.write(txt.toCharArray());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void bufferedWriting() {
+        try(BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(FILE, true), StandardCharsets.UTF_8))) {
+            String txt = "\n이건 Writer로 입력하는 String입니다부디ㅏ문이";
+            writer.write(txt);
+            writer.write(txt);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
